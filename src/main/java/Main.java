4ca2a47f3b4cd58b07a05d9bc4916b82a155cd99
ms1419 <ms1419@ic.ac.uk>
2021-08-15 20:36:27 +0100

    Output CSV

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -10,10 +12,16 @@ public class Main {
             System.out.println("Please provide path to CSV file");
             return;
         }
+
         try {
             List<String> csvLines = Files.readAllLines(Path.of(args[0]));
 
-            PoolToolCSVParser.parseCSV(csvLines.subList(1, csvLines.size())).forEach(System.out::println);
+            List<StakingReward> stakingRewards = PoolToolCSVParser.parseCSV(csvLines.subList(1, csvLines.size()));
+            Map<LocalDate, Float> timestampedPrices = KrakenAPI.getHistoricalPrices();
+
+            List<PricedStakingReward> pricedStakingRewards = stakingRewards.stream().map(stakingReward -> PricedStakingReward.from(stakingReward, timestampedPrices.get(stakingReward.time.toLocalDate()))).toList();
+            System.out.println("Time,Currency,Rewards (ADA),ADA Price (GBP),Rewards (GBP),Description");
+            pricedStakingRewards.forEach(stakingReward -> System.out.println(stakingReward.toCSVLine()));
         } catch (IOException e) {
             e.printStackTrace();
         }
